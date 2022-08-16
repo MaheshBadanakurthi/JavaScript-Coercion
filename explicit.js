21c98c1c73f59d97e06c075cb07b2d420e091dfd
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 18:28:50 +0530

    coercions both are added

---
+++ explicit.js
@@ -0,0 +1,13 @@
+var x=121;
+var y='123';
+if(x==y){
+    console.log('both are equal')
+}
+else{
+    console.log('both are not equal')
+}
+
+let z=x+y;
+// let a=x-y;
+console.log(z,'both are added');
+console.log("bot are not added");
