d63169459d494f00587c09093fd6f4d48782e9b2
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 18:36:46 +0530

    both Coercion are here

--- explicit.js
+++ explicit.js
@@ -8,6 +8,8 @@ else{
 }
 
 let z=x+y;
-// let a=x-y;
-console.log(z,'both are added');
-console.log("bot are not added");
+let a=x-y;  // here system default converted string(123) to number(123). its called Implicit coercion
+var a=Number(y); // here we converted string to number   its called Explicit Coercion.
+console.log(typeof a)
+// console.log(z,'both are added');  // concatination
+// console.log(a,"bot are not added");
